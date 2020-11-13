7c99807cf63fbb3df6290747f635a3490705d72f
qpekejdj <74380848+qpekejdj@users.noreply.github.com>
2020-11-13 16:20:19 +1100

    Add files via upload

---
+++ find big number like pro.py
@@ -0,0 +1,15 @@
+import random
+import time
+a = None
+b = int(input('What nuber to find? '))
+c = len(str(b))
+while a != b:
+    a = random.randint(0,10**c)
+    print(a)
+for i in range(0,10):
+    print('cool hackman')
+    time.sleep(.2)
+    print('mega genius')
+    time.sleep(.2)
+
+
